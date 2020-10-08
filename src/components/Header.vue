36ce37b30cd9d56a3ca93f40c2e6b3d691941168
Zois Pagoulatos <zpagoulatos@hotmail.com>
2020-10-08 23:12:39 +0200

    Mobile header, no menu (WIP)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,9 +9,19 @@
       alt="Tsiafouti.gr"
       href="#top"
     >
-      <div class="ml-8 text-yellow-800 font-semibold"><p>Τσιαφούτη Δήμητρα</p><p>Ψυχολόγος - Ψυχοθεραπεύτρια</p></div>
+      <div class="ml-6 md:ml-8 text-right md:text-left text-yellow-800 font-semibold">
+        <p>Τσιαφούτη Δήμητρα</p><p>Ψυχολόγος - Ψυχοθεραπεύτρια</p>
+      </div>
     </a>
-    <nav class="flex items-center">
+    <!-- <button @click.prevent="showMenu = !showMenu" class="px-2 py-4">
+      <svg v-show="!showMenu" class="visible md:hidden w-6 h-6 -mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+        <path d="M4 6h16M4 12h16M4 18h16"></path>
+      </svg>
+      <svg v-show="showMenu" class="visible md:hidden w-6 h-6 -mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+        <path d="M6 18L18 6M6 6l12 12"></path>
+      </svg>
+    </button> -->
+    <nav class="hidden md:visible md:flex items-center">
       <a href="#bio" class="px-4 font-bold text-gray-800">Βιογραφικό</a>
       <a href="#services" class="px-4 font-bold text-gray-800">Υπηρεσίες</a>
       <a href="#contact" class="px-4 font-bold text-gray-800">Επικοινωνία</a>
@@ -21,7 +31,12 @@
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  data () {
+    return {
+      showMenu: false
+    }
+  }
 }
 </script>
 
