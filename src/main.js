aa9ee10f1ddc671ee99a7b7daddce5287c4aa8c1
Zois Pagoulatos <zpagoulatos@hotmail.com>
2021-01-01 17:16:28 +0100

    Add smooth scroll

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '@/assets/css/tailwind.scss'
+import vueSmoothScroll from 'vue2-smooth-scroll'
 
+Vue.use(vueSmoothScroll)
 Vue.config.productionTip = false
 
 new Vue({
