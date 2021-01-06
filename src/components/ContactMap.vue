c698d9968f41f942b2662bbb51b2e1e17d8e0615
Zois Pagoulatos <zpagoulatos@hotmail.com>
2021-01-06 21:31:52 +0100

    Add maps and contact details

---
+++ src/components/ContactMap.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="mb-6 text-lg">
+      <h5 class="font-semibold mb-6">Στοιχεία επικοινωνίας</h5>
+      <div class="flex text-yellow-800">
+        <svg class="w-4 mr-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+        </svg>
+        <p class="">Ραιδεστού 35, 10441, Αθήνα</p>
+      </div>
+      <div class="flex text-yellow-800">
+        <svg class="w-4 mr-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+        </svg>
+      <p class="">(+30) 690 123 4567</p>
+      </div>
+    </div>
+    <gmap-map
+      :center="center"
+      :zoom="18"
+      class="w-full"
+      style="height: 280px;"
+    >
+      <gmap-marker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+      ></gmap-marker>
+      <gmap-info-window :position="center">
+        <p class="font-bold mb-1">Τσιαφούτη Δήμητρα</p>
+        <p>Ραιδεστού 35, 10441, Αθήνα</p>
+      </gmap-info-window>
+    </gmap-map>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactMap',
+  data () {
+    return {
+      center: {},
+      markers: []
+    }
+  },
+
+  mounted () {
+    const center = { lat: 37.99604, lng: 23.716309 }
+
+    this.center = center
+    this.markers.push({ position: { ...center } })
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
