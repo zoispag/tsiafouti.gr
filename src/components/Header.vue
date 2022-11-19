02de33864f63bff72eeac3a6cec955cfca20d48c
Zois Pagoulatos <zpagoulatos@hotmail.com>
2022-11-19 16:04:11 +0200

    Change text next to logo (#43)
    * Change text next to logo
    
    * Trial
    
    * Font size
    
    * Text nowrap
    
    * No wrap on desktop
    
    * Add MSc

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,15 +3,16 @@
     class="flex justify-between px-8 py-6 top-0 sticky overflow-hidden border-yellow-800 border-t-4 z-50"
     id="navbar"
   >
-    <a v-smooth-scroll href="#hero" class="flex justify-between w-full md:w-80"><img
+    <a v-smooth-scroll href="#hero" class="flex justify-between w-full md:w-1/3"><img
       class="h-12 object-cover"
       src="~@/assets/media/logo_head.png"
       alt="Tsiafouti.gr"
       href="#top"
     >
-      <div class="ml-6 md:ml-8 text-right md:text-left ">
-        <p class="font-bold">Τσιαφούτη Δήμητρα</p>
-        <p class="font-normal text-yellow-800">Ψυχολόγος - Ψυχοθεραπεύτρια</p>
+      <div class="ml-6 md:ml-8 text-right md:text-left">
+        <p class="font-normal text-sm text-gray-700">Χώρος ψυχοθεραπείας και συμβουλευτικής</p>
+        <p class="font-bold">Τσιαφούτη Δήμητρα, MSc</p>
+        <p class="font-normal text-sm text-yellow-800 md:whitespace-nowrap">Ψυχολόγος - Ψυχοθεραπεύτρια παιδιού και εφήβου, οικογενειακή θεραπεύτρια</p>
       </div>
     </a>
     <nav class="hidden md:visible md:flex items-center">
