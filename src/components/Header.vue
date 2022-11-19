8c53a840e6713b412b815074e73aec79e0c91bc9
Zois Pagoulatos <zpagoulatos@hotmail.com>
2022-11-19 15:55:31 +0200

    No wrap on desktop

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,7 @@
       <div class="ml-6 md:ml-8 text-right md:text-left">
         <p class="font-normal text-sm text-gray-700">Χώρος ψυχοθεραπείας και συμβουλευτικής</p>
         <p class="font-bold">Τσιαφούτη Δήμητρα</p>
-        <p class="font-normal text-sm text-yellow-800 whitespace-nowrap">Ψυχολόγος - Ψυχοθεραπεύτρια παιδιού και εφήβου, οικογενειακή θεραπεύτρια</p>
+        <p class="font-normal text-sm text-yellow-800 md:whitespace-nowrap">Ψυχολόγος - Ψυχοθεραπεύτρια παιδιού και εφήβου, οικογενειακή θεραπεύτρια</p>
       </div>
     </a>
     <nav class="hidden md:visible md:flex items-center">
