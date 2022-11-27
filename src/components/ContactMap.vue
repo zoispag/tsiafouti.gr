93ddf2b7a53676ba34bae66f0485b5e38ee05bd3
Zois Pagoulatos <zpagoulatos@hotmail.com>
2022-11-27 13:51:34 +0200

    update phone number

--- src/components/ContactMap.vue
+++ src/components/ContactMap.vue
@@ -20,7 +20,7 @@ const center = { lat: 37.9828407, lng: 23.7596172 }
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
             d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
         </svg>
-        <a class="flex-1" href="tel:+306901234567">(+30) 690 123 4567</a>
+        <a class="flex-1" href="tel:+306987896063">(+30) 698 789 6063</a>
       </div>
       <div class="flex text-yellow-800">
         <svg class="w-5 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
