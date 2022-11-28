fff3e67ea15c42fc3dca4f523bf9e6bcd23bedf6
Zois Pagoulatos <zpagoulatos@hotmail.com>
2022-11-28 17:12:16 +0200

    Punchline with A/B testing and CSS mix-blend (#53)

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,28 @@
+<script setup>
+import { computed } from 'vue'
+
+const punchlines = {
+  en: "When I accept myself just as I am, then I can change",
+  gr: "Όταν αποδεχτώ τον εαυτό μου όπως είναι, τότε μπορώ να αλλάξω",
+}
+const origins = {
+  en: "Carl Rogers",
+  gr: "Καρλ Ρότζερς",
+}
+
+const lang = computed(() => Math.floor(Math.random() * 10) < 6 ? 'gr' : 'en') // 60% GR - 40% EN
+
+const punchline = computed(() => punchlines[lang.value])
+const origin = computed(() => origins[lang.value])
+
+</script>
+
 <template>
   <div id="hero" class="-mt-24">
     <div class="">
-      <div class="absolute mt-48 md:mt-64 right-0 pr-32 text-xl sm:text-2xl md:text-4xl">
-        <div class="text-right text-gray-600 italic">"Once we accept our limits, we go beyond them"</div>
-        <div class="text-right text-gray-700 text-lg sm:text-xl md:text-3xl">— Einstein</div>
+      <div class="absolute mt-48 md:mt-64 right-0 pr-4 md:pr-32 text-xl md:text-md lg:text-2xl">
+        <div class="text-right mix-blend-difference text-slate-400 italic">"{{ punchline }}"</div>
+        <div class="text-right mix-blend-difference text-slate-400 text-lg sm:text-xl md:text-2xl">— {{ origin }}</div>
       </div>
       <img class="w-full h-screen object-cover" src="~@/assets/media/photo-1487700160041-babef9c3cb55.jpeg">
     </div>
