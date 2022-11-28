32dc2b484818397473ca659f20941b761c806138
Zois Pagoulatos <zpagoulatos@hotmail.com>
2022-11-28 12:05:48 +0200

    Standardize SVG icons across top header and contact (#55)
    * Standardize SVG icons across top header and contact
    
    * Use new heroicons

--- src/components/AboveHeader.vue
+++ src/components/AboveHeader.vue
@@ -6,12 +6,12 @@ import EmailIconVue from './icons/EmailIcon.vue';
 <template>
   <div class="flex flex-col md:flex-row gap-4 px-8 py-2 md:pb-1 md:pt-2 bg-yellow-800">
     <section class="flex items-center text-sm font-normal md:font-bold text-white gap-3">
-      <AddressIconVue />
+      <AddressIconVue class="w-5 h-5" />
       <span>Έβρου 36, 11527, Αμπελόκηποι, Αθήνα</span>
     </section>
 
     <section class="flex items-center text-sm font-normal md:font-bold text-white gap-3">
-      <EmailIconVue />
+      <EmailIconVue class="w-5 h-5" />
       <a href="mailto:d.tsiafouti@gmail.com">d.tsiafouti@gmail.com</a>
     </section>
 
