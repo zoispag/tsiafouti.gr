9f003d5a4be16d227e2473d7beab8e25c88d957e
Zois Pagoulatos <zpagoulatos@hotmail.com>
2022-11-28 12:03:27 +0200

    Use new heroicons

--- src/components/ContactMap.vue
+++ src/components/ContactMap.vue
@@ -11,15 +11,15 @@ const center = { lat: 37.9828407, lng: 23.7596172 }
   <div>
     <address class="mb-6 text-lg not-italic">
       <h5 class="font-semibold mb-6">Στοιχεία επικοινωνίας</h5>
-      <div class="flex text-yellow-800">
+      <div class="flex items-center text-yellow-800">
         <AddressIcon class="w-5 mr-3" />
         <p class="flex-1">Έβρου 36, 11527, Αμπελόκηποι, Αθήνα</p>
       </div>
-      <div class="flex text-yellow-800">
+      <div class="flex items-center text-yellow-800">
         <TelephoneIcon class="w-5 mr-3" />
         <a class="flex-1" href="tel:+306987896063">(+30) 698 789 6063</a>
       </div>
-      <div class="flex text-yellow-800">
+      <div class="flex items-center text-yellow-800">
         <EmailIcon class="w-5 mr-3" />
         <a class="flex-1" href="mailto:d.tsiafouti@gmail.com">d.tsiafouti@gmail.com</a>
       </div>
